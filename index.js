c52a7b52dd43467b1f338083315af3e3f88a2bcb
Benjamin <Officialbenragland@gmail.com>
2024-02-11 01:29:03 -0500

    Adds Script to index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+const main = document.querySelector("main")
+main.remove()
+//Deletes main
+
+
+const newHeader = document.createElement('h1')
+newHeader.id="victory"
+newHeader.textContent="BEN-RAGLAND is the champion"
+
+
